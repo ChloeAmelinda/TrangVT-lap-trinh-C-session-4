f0e7086b0203842f2317c2b91cfb35fc797b9676
ChloeAmelinda <trangthichngunuong@gmail.com>
2024-11-21 23:20:58 +0700

    ss4 bai 2

---
+++ ss4 bai 2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Moi ban nhap so bat ki :");
+	scanf("%d", &n);
+	
+	if(n%2==0)
+	{
+		printf("La so chan ");
+	}
+	
+	else 
+	{
+	printf("La so le");
+	}
+	
+	return 0;
+	
+}
